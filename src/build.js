a98f0bac103e2e9d356646456dab22c3c1832292
Brent Jackson <jxnblk@github.com>
2015-02-20 17:38:29 -0500

    NPM scripts

--- src/build.js
+++ src/build.js
@@ -9,9 +9,9 @@ require('node-jsx').install();
 var template = _.template(fs.readFileSync(path.join(__dirname, './layouts/default.html'), 'utf8'));
 var App = React.createFactory(require('./components/App.jsx'));
 
-module.exports = function(data) {
-  data.app = React.renderToString(App(data));
-  var html = template(data);
-  fs.writeFileSync(path.join(__dirname, '../index.html'), html);
-};
+var data = require('./data');
+
+data.app = React.renderToString(App(data));
+var html = template(data);
+fs.writeFileSync(path.join(__dirname, '../index.html'), html);
 
