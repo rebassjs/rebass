9f42d07ab39f5887d5fd96fbdfad1d89c80f75e8
Brent Jackson <jacksonblack@gmail.com>
2015-03-08 13:28:30 -0400

    Set baseurl for gh-pages

--- docs/js/app.js
+++ docs/js/app.js
@@ -2020,7 +2020,7 @@ if (process.env.NODE_ENV === 'production') {
   data.baseUrl = '/rebass/';
 } else {
   console.log('development build');
-  //data.baseUrl = '/rebass/';
+  data.baseUrl = '/rebass/';
 }
 
 data.markdown = {
