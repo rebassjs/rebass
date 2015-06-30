0c9e5119b58ca55be35553de039f3917829153e6
jxnblk <jacksonblack@gmail.com>
2015-06-30 15:36:11 -0400

    Rename files

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,7 @@ module.exports = {
 
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
+      { test: /(\.js$|\.jsx?$)/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.css/, loader: 'css-loader!cssnext-loader' }
     ]
