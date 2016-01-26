b245abbfcd4adb809b4ee207a28dafff63753ed0
Joe Kendall <call@1800joe.com>
2016-01-25 22:59:05 -0500

    Updating text var so children can be included

--- src/PanelHeader.jsx
+++ src/PanelHeader.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 class PanelHeader extends React.Component {
 
   render () {
-    let text = <h4 className='m0'>{this.props.title}</h4> || this.props.children
+    let text = this.props.text ? <h4 className='m0'>{this.props.title}</h4> : this.props.children
     return (
       <div className='p2 white bg-blue rounded-top'>
         {text}
