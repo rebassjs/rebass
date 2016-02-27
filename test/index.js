1b500acbc9a76c97d9d11cad4c201ebe26a4315b
jxnblk <jacksonblack@gmail.com>
2016-02-27 12:15:22 -0500

    Test scaffolding

---
+++ test/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import expect from 'expect'
+import { Button } from '../src'
+
+const renderer = TestUtils.createRenderer()
+
+describe('Button', () => {
+
+  it('should render', () => {
+    renderer.render(<Button />)
+    const tree = renderer.getRenderOutput()
+    expect(tree.type).toEqual('button')
+  })
+})
+
