a351c03558441059cf62560b16065f2089c4d042
jxnblk <jacksonblack@gmail.com>
2016-03-01 13:59:38 -0500

    Fix basic context tests

--- test/context.js
+++ test/context.js
@@ -6,56 +6,33 @@ import { Button, Panel } from '../src'
 
 const renderer = TestUtils.createRenderer()
 
-class Root extends React.Component {
-  static childContextTypes = {
-    rebass: React.PropTypes.object
-  }
-
-  getChildContext () {
-    return {
-      rebass: {
-        colors: {
-          primary: 'red'
-        },
-        Button: {
-          backgroundColor: 'tomato'
-        }
-      }
-    }
-  }
-
-  render () {
-    return (
-      <div>
-        <Button />
-        <Panel />
-      </div>
-    )
-  }
-}
-
 describe('React context', () => {
-  let tree, button, panel
+  let tree
 
   beforeEach(() => {
-    renderer.render(
-      <Root>
-        <Button />
-        <Panel />
-      </Root>
-    )
+    renderer.render(<Button />, {
+      rebass: {
+        colors: {
+          primary: 'tomato'
+        },
+        Button: {
+          textTransform: 'uppercase'
+        }
+      }
+    })
     tree = renderer.getRenderOutput()
-    button = tree.props.children[0]
-    panel = tree.props.children[1]
   })
 
   it('should render', () => {
-    expect(tree.type).toEqual('div')
-    expect(button).toExist()
+    expect(tree.type).toEqual('button')
   })
 
-  it('should pick up custom styles from context')
+  it('should pick up custom styles from context', () => {
+    expect(tree.props.style.textTransform).toEqual('uppercase')
+  })
 
-  it('should pick up custom global styles from context')
+  it('should pick up custom global styles from context', () => {
+    expect(tree.props.style.backgroundColor).toEqual('tomato')
+  })
 })
 
