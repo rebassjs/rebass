fe18bb5d2898176d0b45bcb99974a2ec86c06df1
jxnblk <jacksonblack@gmail.com>
2016-03-03 18:29:23 -0500

    Use Base component in Heading

--- src/Heading.js
+++ src/Heading.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import Base from './Base'
 import theme from './theme'
 
 /**
@@ -10,37 +11,34 @@ const Heading =({
   level,
   size,
   big,
-  style,
   ...props
 }, { rebass }) => {
   const config = { ...theme, ...rebass }
-  const headingStyle = rebass ? rebass.Heading : {}
   const { bold } = config
   const Component = `h${level}`
   const fontSize = (typeof size === 'number' ? theme.fontSizes[size] : theme.fontSizes[level]) * (big ? 2 : 1)
 
   return (
-    <Component
+    <Base
       {...props}
+      tagName={Component}
       className='Heading'
-      style={{
+      baseStyle={{
         fontSize,
         fontWeight: bold,
         lineHeight: 1.25,
-        margin: 0,
-        ...headingStyle,
-        ...style
+        margin: 0
       }} />
   )
 }
 
 Heading.propTypes = {
+  /** Doubles the visual size - useful for marketing pages */
+  big: React.PropTypes.bool,
   /** Heading level, e.g. level={1} for <h1> */
   level: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
   /** Visual size of heading */
   size: React.PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
-  /** Doubles the visual size - useful for marketing pages */
-  big: React.PropTypes.bool
 }
 
 Heading.defaultProps = {
