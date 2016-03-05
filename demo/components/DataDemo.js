776dffb02386d4e71305ba6f4c58dd25a47bea6d
jxnblk <jacksonblack@gmail.com>
2016-03-05 18:36:44 -0500

    Add interactive Donut demo

--- demo/components/DataDemo.js
+++ demo/components/DataDemo.js
@@ -2,66 +2,151 @@
 import React from 'react'
 import { Flex, Box } from 'reflexbox'
 import {
+  Block,
   Divider,
   Donut,
   Section,
   SectionHeader,
+  Heading,
+  Slider,
+  Space,
   Stat,
 } from '../../src'
 
-const DataDemo = ({ repo, version, ...props }) => (
-  <Section>
-    <SectionHeader heading='Stats' />
-    <Flex
-      wrap
-      gutter={2}
-      justify='space-between'
-      align='center'>
-      <Donut
-        color='info'
-        m={2}
-        size={256}
-        strokeWidth={16}
-        value={5/8} />
-      <Donut
-        color='green'
-        m={2}
-        size={192}
-        strokeWidth={16}
-        value={7/8} />
-      <Donut
-        color='error'
-        m={2}
-        size={192}
-        strokeWidth={16}
-        value={1/16} />
-    </Flex>
-    <Divider />
-    <Flex
-      wrap
-      justify='space-between'
-      gutter={2}
-      align='center'>
-      <Stat
-        m={2}
-        label='Stars'
-        color='orange'
-        value={repo.stargazers_count || '...'} />
-      <Stat
-        m={2}
-        label='Forks'
-        value={repo.forks_count || '...'} />
-      <Stat
-        m={2}
-        label='Watchers'
-        value={repo.watchers_count || '...'} />
-      <Stat
-        m={2}
-        label='Version'
-        value={version} />
-    </Flex>
-  </Section>
-)
+const rad = (a) => Math.PI * a /180
+
+const ry = (r, a) => r * Math.cos(rad(a))
+const rx = (r, a) => r * Math.sin(rad(a))
+
+class DataDemo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      angle: 60,
+      radius: 128,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render () {
+    const { repo, version } = this.props
+    const { radius, angle } = this.state
+
+    const x = rx(radius, angle)
+    const y = ry(radius, angle)
+    const maxRadius = 320
+
+    return (
+      <Section>
+        <SectionHeader heading='Stats' />
+        <Flex
+          wrap
+          justify='space-between'
+          align='stretch'>
+          <Flex
+            column
+            align='center'
+            style={{
+              width: 320,
+              minHeight: 384,
+            }}>
+            <Space auto />
+            <Donut
+              color='info'
+              size={radius}
+              strokeWidth={16}
+              children={`${radius}px`}
+              value={radius / maxRadius} />
+            <Space auto />
+            <Slider
+              color='info'
+              fill
+              style={{ width: '100%' }}
+              name='radius'
+              label='Radius'
+              value={radius}
+              min={128}
+              max={maxRadius}
+              onChange={this.handleChange} />
+          </Flex>
+          <Flex column>
+            <Space auto />
+            <Donut
+              color='green'
+              size={192}
+              strokeWidth={16}
+              children={`${angle}°`}
+              value={angle / 360} />
+            <Space auto />
+            <Slider
+              color='green'
+              fill
+              style={{ width: '100%' }}
+              name='angle'
+              label='Angle'
+              value={angle}
+              max={360}
+              onChange={this.handleChange} />
+          </Flex>
+          <Box sm={3}>
+            <Block
+              style={{ textAlign: 'right' }}
+              pr={0}
+              color='red'
+              borderColor='red'
+              borderLeft>
+              <Heading level={4}
+                size={2}
+                children='Coordinates' />
+              <div>
+                <Stat
+                  m={2}
+                  topLabel
+                  label='X'
+                  value={`${Math.round(x)}px`} />
+              </div>
+              <div>
+                <Stat
+                  m={2}
+                  topLabel
+                  label='Y'
+                  value={`${Math.round(y)}px`} />
+              </div>
+            </Block>
+          </Box>
+        </Flex>
+        <Divider />
+        <Flex
+          wrap
+          justify='space-between'
+          gutter={2}
+          align='center'>
+          <Stat
+            m={2}
+            label='Stars'
+            color='orange'
+            value={repo.stargazers_count || '...'} />
+          <Stat
+            m={2}
+            label='Forks'
+            value={repo.forks_count || '...'} />
+          <Stat
+            m={2}
+            label='Watchers'
+            value={repo.watchers_count || '...'} />
+          <Stat
+            m={2}
+            label='Version'
+            value={version} />
+        </Flex>
+      </Section>
+    )
+  }
+}
 
 export default DataDemo
 
