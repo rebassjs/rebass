e82f1d1c071097fabf784b1e1b03e553884e91c1
jxnblk <jacksonblack@gmail.com>
2016-09-18 13:09:12 -0400

    Use HOC

--- src/Panel.js
+++ src/Panel.js
@@ -1,61 +1,57 @@
 
 import React from 'react'
-import Base from './Base'
-import config from './config'
+import classnames from 'classnames'
+import withRebass from './withRebass'
 
 /**
  * Panel for containing small pieces of information
  */
 
-const Panel = ({ theme, children, ...props }, { rebass }) => {
-  const { scale, colors, borderRadius } = { ...config, ...rebass }
+const Panel = ({
+  children,
+  className,
+  style,
+  theme,
+  subComponentStyles,
+  ...props
+}) => {
+  const { scale, colors, borderRadius } = theme
 
-  const borderColor = colors[theme]
+  const cx = classnames('Panel', className)
+
+  const borderColor = colors.primary // colors[theme]
+
+  const sx = {
+    padding: scale[2],
+    marginBottom: scale[2],
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor,
+    borderRadius,
+    backgroundColor: colors.white,
+    ...style
+  }
+
   const styledChildren = React.Children.map(children, (child) => {
+    // To do: handle theme props
     if (child && child.props && child.props.theme === 'default') {
-      return React.cloneElement(child, { theme })
+      return React.cloneElement(child, {
+        // color,
+        // backgroundColor
+      })
     } else {
       return child
     }
   })
 
   return (
-    <Base
+    <div
       {...props}
-      className='Panel'
-      baseStyle={{
-        padding: scale[2],
-        marginBottom: scale[2],
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor,
-        borderRadius,
-        backgroundColor: colors.white
-      }}
+      className={cx}
+      style={sx}
       children={styledChildren} />
   )
 }
 
-Panel.propTypes = {
-  /** Sets color from config */
-  theme: React.PropTypes.oneOf([
-    'primary',
-    'secondary',
-    'default',
-    'info',
-    'success',
-    'warning',
-    'error'
-  ])
-}
-
-Panel.defaultProps = {
-  theme: 'default'
-}
-
-Panel.contextTypes = {
-  rebass: React.PropTypes.object
-}
-
-export default Panel
+export default withRebass(Panel)
 
