e439fc45293433214892cfe299e6fcece1c7a242
Brent Jackson <jxnblk@gmail.com>
2016-10-01 11:22:27 -0400

    Add accordion panel for editable props

--- docs/ComponentDetail.js
+++ docs/ComponentDetail.js
@@ -9,14 +9,12 @@ import {
   Heading,
   Text,
   Pre,
-  Input,
-  Select,
-  Checkbox,
   theme
 } from '../src'
 import BigHeading from './BigHeading'
 import Rule from './Rule'
 import PropsTable from './PropsTable'
+import ComponentPropsForm from './ComponentPropsForm'
 import getExample, { filterNull } from './examples'
 import data from './data.json'
 
@@ -28,7 +26,7 @@ const PropField = ({
 
 class ComponentDetail extends React.Component {
   state = {
-    text: 'Hello Rebass',
+    text: '', // 'Hello Rebass',
     number: 16,
     image: null,
     theme: null,
@@ -39,6 +37,7 @@ class ComponentDetail extends React.Component {
     pill: false,
     color: null,
     backgroundColor: null,
+    propsOpen: false
   }
 
   onChange = obj => {
@@ -47,23 +46,23 @@ class ComponentDetail extends React.Component {
 
   handleChange = e => {
     const { name, value } = e.target
-    const val = value === '' ? null : value
+    console.log(value === -1, value === '-1')
+    const val = (value === '' || value === '-1')
+      ? null
+      : /^true$|^false$/.test(value)
+      ? value === 'true'
+      : value
     this.setState({ [name]: val })
+  }
+
+  togglePropsOpen = () => {
+    const propsOpen = !this.state.propsOpen
+    this.setState({ propsOpen })
   }
 
   render () {
     const { params } = this.props
-    const {
-      text,
-      image,
-      theme,
-      m,
-      p,
-      rounded,
-      pill,
-      color,
-      backgroundColor
-    } = this.state
+    const { text } = this.state
     const { name } = params
     const comp = find(data.components, d => d.name === name)
 
@@ -94,7 +93,7 @@ class ComponentDetail extends React.Component {
           <Text children={comp.description} />
           <Rule />
         </Grid>
-        <Grid span={6}>
+        <Grid span={8}>
           <Heading
             alt
             mb={3}
@@ -103,139 +102,17 @@ class ComponentDetail extends React.Component {
           </Heading>
           {example}
         </Grid>
-        <Grid span={1}>
-          <Input
-            name='text'
-            label='text'
-            mb={1}
-            value={text}
-            onChange={this.handleChange} />
-          {/*
-            <Select
-              name='image'
-              label='image'
-              value={image || ''}
-              onChange={this.handleChange}
-              options={[
-                { value: '', children: 'Space' },
-              ]}
-            />
-          */}
-          <Select
-            name='color'
-            label='color'
-            mb={1}
-            value={color || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: 'white', children: 'white' },
-              { value: 'black', children: 'black' },
-              { value: 'blue', children: 'blue' },
-              { value: 'green', children: 'green' },
-              { value: 'orange', children: 'orange' },
-              { value: 'red', children: 'red' },
-              { value: 'midgray', children: 'midgray' },
-              { value: 'gray', children: 'gray' },
-            ]}
-          />
-          <Select
-            name='backgroundColor'
-            label='backgroundColor'
-            mb={1}
-            value={backgroundColor || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: 'white', children: 'white' },
-              { value: 'black', children: 'black' },
-              { value: 'blue', children: 'blue' },
-              { value: 'green', children: 'green' },
-              { value: 'orange', children: 'orange' },
-              { value: 'red', children: 'red' },
-              { value: 'midgray', children: 'midgray' },
-              { value: 'gray', children: 'gray' },
-            ]}
-          />
-          <Select
-            name='theme'
-            label='fill'
-            mb={1}
-            value={theme || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: 'primary', children: 'primary' },
-              { value: 'default', children: 'default' },
-              { value: 'muted', children: 'muted' },
-              { value: 'info', children: 'info' },
-              { value: 'success', children: 'success' },
-              { value: 'warning', children: 'warning' },
-              { value: 'error', children: 'error' }
-            ]}
-          />
+        <Grid span={3}>
+          <Pre children={code} />
         </Grid>
-        <Grid span={1}>
-          <Select
-            name='m'
-            label='margin'
-            mb={1}
-            value={m || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: 0, children: '0' },
-              { value: 1, children: '1' },
-              { value: 2, children: '2' },
-              { value: 3, children: '3' },
-              { value: 4, children: '4' },
-            ]}
-          />
-          <Select
-            name='p'
-            label='padding'
-            mb={1}
-            value={p || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: 0, children: '0' },
-              { value: 1, children: '1' },
-              { value: 2, children: '2' },
-              { value: 3, children: '3' },
-              { value: 4, children: '4' },
-            ]}
-          />
-          <Select
-            name='rounded'
-            label='rounded'
-            mb={1}
-            value={rounded || ''}
-            onChange={this.handleChange}
-            options={[
-              { value: '', children: 'null' },
-              { value: false, children: 'false' },
-              { value: true, children: 'true' },
-              { value: 'top', children: 'top' },
-              { value: 'bottom', children: 'bottom' },
-              { value: 'left', children: 'left' },
-              { value: 'right', children: 'right' },
-            ]}
-          />
-          {/*
-            <Checkbox
-              name='pill'
-              label='pill'
-              checked={pill}
-              onClick={e => { this.onChange({ pill: !pill }) }}
-            />
-          */}
+        <Grid span={2}>
+          <ComponentPropsForm
+            {...this.state}
+            toggle={this.togglePropsOpen}
+            onChange={this.handleChange} />
         </Grid>
         <Grid span={3}>
           <PropsTable props={comp.props} />
-        </Grid>
-        <Grid span={3}>
-          <Pre children={code} />
         </Grid>
         <Grid span={6}>
         </Grid>
