40e429a4cf1e858d8b1d5d04266ee0f033584493
Brent Jackson <jxnblk@gmail.com>
2017-07-04 18:26:20 -0400

    Fix color key

--- docs/examples/Banner.jsx
+++ docs/examples/Banner.jsx
@@ -1,6 +1,6 @@
 <Banner
   color='white'
-  bg='blue.8'
+  bg='gray8'
   backgroundImage='https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'>
   <Heading
     f={[ 4, 5, 6, 7 ]}>
