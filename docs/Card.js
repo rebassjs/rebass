1105145368e6f053288234771863a3148b745caa
Brent Jackson <jxnblk@gmail.com>
2017-07-04 17:12:48 -0400

    Add card images

---
+++ docs/Card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Provider, Banner } from '../src'
+import Logo from './Logo'
+
+const photo = 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'
+
+const Card = props => (
+  <Provider>
+    <Banner
+      backgroundImage={photo}
+      style={{
+        width: 768,
+        minHeight: 0,
+        height: 576
+      }}>
+      <Logo
+        size={512}
+        static
+      />
+    </Banner>
+  </Provider>
+)
+
+export default Card
--- docs/Logo.js
+++ docs/Logo.js
@@ -2,13 +2,6 @@ import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { theme } from '../src'
 
-const Placeholder = styled.div([], {
-  width: '256px',
-  height: '256px',
-  backgroundColor: '#eee',
-  borderRadius: '9999px'
-})
-
 const Svg = styled.svg`
   display: block;
   max-width: 100%;
@@ -18,16 +11,10 @@ const Svg = styled.svg`
   mix-blend-mode: overlay;
 
   @media screen and (min-width: ${theme.breakpoints[1]}em) {
-    width: 320px;
-    height: 320px;
+    width: ${props => props.size ? props.size + 'px' : '320px'};
+    height: ${props => props.size ? props.size + 'px' : '320px'};
   }
 `
-// @media screen and (min-width: ${theme.breakpoints[3]}em) {
-//     width: 448px;
-//     height: 448px;
-//   }
-
-// 0% { transform: rotate3d(0, 1, 1, 360deg) }
 
 const spin1 = keyframes`
   50% { transform: rotate3d(0, 1, 1, 180deg) }
@@ -54,38 +41,60 @@ const Electron2 = styled.circle`
   animation-iteration-count: infinite;
 `
 
-const Logo = props => (
-  <Svg viewBox='0 0 64 64'
-    width={props.size}
-    height={props.size}>
-    <circle
-      cx={32}
-      cy={32}
-      r={24}
-      strokeWidth={4}
-      vectorEffect='non-scaling-stroke'
-    />
-    <Electron1
-      cx={32}
-      cy={32}
-      r={20}
-      strokeWidth={2}
-      vectorEffect='non-scaling-stroke'
-    />
-    <Electron2
-      cx={32}
-      cy={32}
-      r={20}
-      strokeWidth={2}
-      vectorEffect='non-scaling-stroke'
-    />
-    <path
-      d='M0 32 H64'
-      strokeWidth={1}
-      vectorEffect='non-scaling-stroke'
-    />
-  </Svg>
-)
+const ElectronStatic1 = styled.circle`
+  transform-origin: 50% 50%;
+  transform: rotate3d(0, 1, 1, 90deg);
+`
+
+const ElectronStatic2 = styled.circle`
+  transform-origin: 50% 50%;
+  transform: rotate3d(1, 0, 1, 90deg);
+`
+
+const Logo = props => {
+  const electronProps = {
+    cx: 32,
+    cy: 32,
+    r: 20,
+    strokeWidth: 2,
+    vectorEffect: 'non-scaling-stroke'
+  }
+
+  const electrons = props.static
+    ? (
+      <g>
+        <ElectronStatic1 {...electronProps} />
+        <ElectronStatic2 {...electronProps} />
+      </g>
+    )
+    : (
+      <g>
+        <Electron1 {...electronProps} />
+        <Electron2 {...electronProps} />
+      </g>
+    )
+
+  return (
+    <Svg viewBox='0 0 64 64'
+      size={props.size}
+      width={props.size}
+      height={props.size}>
+      <circle
+        cx={32}
+        cy={32}
+        r={24}
+        strokeWidth={4}
+        vectorEffect='non-scaling-stroke'
+      />
+      {electrons}
+      <path
+        d='M0 32 H64'
+        strokeWidth={1}
+        vectorEffect='non-scaling-stroke'
+      />
+    </Svg>
+  )
+}
 
 Logo.defaultProps = {
   size: 256
