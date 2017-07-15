08a2225cb999d3eddf74a6cfcb1bcf34cbee8f7b
Brent Jackson <jxnblk@gmail.com>
2017-07-15 15:13:15 -0400

    Remove xmlns attribute

--- src/SelectBase.js
+++ src/SelectBase.js
@@ -23,7 +23,6 @@ const Select = styled.select([], {
 const Arrow = styled(props => (
   <svg
     {...props}
-    xmlns='http://www.w3.org/2000/svg'
     viewBox='0 0 32 32'>
     <path
       d='M0 6 L32 6 L16 28 z'
