40620385bb0899f2b5403244d186b61c5070babd
Brent Jackson <jxnblk@gmail.com>
2018-02-19 11:57:43 -0500

    Apostrophe

--- docs/src/About.js
+++ docs/src/About.js
@@ -12,7 +12,7 @@ const About = props => (
         Rebass is a library of highly-composable, primitive UI components for React,
         built with styled-components to keep styles isolated and reduce the need to write custom CSS in your application.
         Based upon a configurable design system,
-        Rebass‘s props API makes building consistent, responsive web apps simpler and faster.
+        Rebass’s props API makes building consistent, responsive web apps simpler and faster.
       </Lead>
     </Box>
   </Container>
