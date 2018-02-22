2b1add8c0c2ddbb202b5d8b55c78c7d945831396
Brent Jackson <jxnblk@gmail.com>
2018-02-21 23:28:46 -0500

    Undo Pre changes

--- src/Pre.js
+++ src/Pre.js
@@ -4,11 +4,8 @@ export const Pre = sys({
   is: 'pre',
   fontSize: 1,
   fontFamily: 'mono',
-  width: 1,
-  maxWidth: '100%',
   m: 0
 }, {
-  display: 'block',
   overflow: 'auto'
 },
   'fontFamily',
