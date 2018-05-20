4ff21fe2ed5936cd8e1b89a1386fe8d9cec27f9b
Brent Jackson <jxnblk@gmail.com>
2018-05-20 10:58:27 -0400

    Move tests folder in basic example

--- examples/basic/tests/snapshots.js
+++ examples/basic/tests/snapshots.js
@@ -1,5 +1,5 @@
 import snapshot from '@compositor/kit-snapshot'
 import 'jest-styled-components'
-import * as examples from '../../examples'
+import * as examples from '../examples'
 
 snapshot(examples)
