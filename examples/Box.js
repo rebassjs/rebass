70e489f8cfaf00191f6a97760696efb0cf59c18e
Brent Jackson <jxnblk@gmail.com>
2018-05-28 15:14:41 -0400

    Remove placeholder examples

--- examples/Box.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Box } from '../src'
-
-export default () => (
-  <Box p={4} color='white' bg='blue'>
-    Box
-  </Box>
-)
--- examples/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button } from '../src'
-
-export default () => (
-  <Button>Button</Button>
-)
