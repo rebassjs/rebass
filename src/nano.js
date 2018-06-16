23b68203d17b63dce6b20b4f5870c950ddf37de8
Brent Jackson <jxnblk@gmail.com>
2018-06-16 18:43:10 -0400

    Remove nano and update deps

--- src/nano.js
+++
@@ -1,8 +0,0 @@
-export { Flex, Box } from 'grid-styled'
-export { default as theme } from './theme'
-export { Provider } from './Provider'
-export { Base } from './Base'
-export { Text } from './Text'
-export { Heading } from './Heading'
-export { Button } from './Button'
-export { Link } from './Link'
