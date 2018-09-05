0a888d5581d61c1a5c1787de2f7890bcc8236ea2
Brent Jackson <jxnblk@gmail.com>
2018-09-04 22:28:23 -0400

    Rewrite

---
+++ src/index.js
@@ -0,0 +1,199 @@
+import React from 'react'
+import styled from 'react-emotion'
+import {
+  space,
+  color,
+  width,
+  fontSize,
+  fontFamily,
+  fontWeight,
+  textAlign,
+  lineHeight,
+  letterSpacing,
+  borders,
+  borderColor,
+  borderRadius,
+  buttonStyle,
+
+  boxShadow,
+  backgroundImage,
+  backgroundSize,
+  backgroundPosition,
+  backgroundRepeat,
+  opacity,
+} from 'styled-system'
+import tag from 'clean-tag'
+
+export { Box, Flex } from '@rebass/grid/emotion'
+
+export const css = props => props.css
+export const themed = key => props => props.theme[key]
+
+// theme
+
+
+export const Text = styled(tag)(
+  space,
+  color,
+  fontSize,
+  fontFamily,
+  fontWeight,
+  textAlign,
+  lineHeight,
+  letterSpacing,
+  themed('Text'),
+  css
+)
+
+Text.propTypes = {
+  ...space.propTypes,
+  ...color.propTypes,
+  ...fontSize.propTypes,
+  ...fontFamily.propTypes,
+  ...fontWeight.propTypes,
+  ...textAlign.propTypes,
+  ...lineHeight.propTypes,
+  ...letterSpacing.propTypes,
+}
+
+// export const Heading = Text.withComponent('h2')
+// export const Heading = styled(Text)().withComponent('h2')
+
+export const Heading = styled(tag.h2)(
+  space,
+  color,
+  fontSize,
+  fontFamily,
+  fontWeight,
+  textAlign,
+  lineHeight,
+  letterSpacing,
+  themed('Heading'),
+  css
+)
+
+Heading.propTypes = {
+  ...space.propTypes,
+  ...color.propTypes,
+  ...fontSize.propTypes,
+  ...fontFamily.propTypes,
+  ...fontWeight.propTypes,
+  ...textAlign.propTypes,
+  ...lineHeight.propTypes,
+  ...letterSpacing.propTypes,
+}
+
+Heading.defaultProps = {
+  m: 0,
+  fontSize: 4,
+  fontWeight: 'bold',
+}
+
+Heading.h1 = props => <Heading is='h1' {...props} />
+Heading.h2 = props => <Heading is='h2' {...props} />
+Heading.h3 = props => <Heading is='h3' {...props} />
+Heading.h4 = props => <Heading is='h4' {...props} />
+Heading.h5 = props => <Heading is='h5' {...props} />
+Heading.h6 = props => <Heading is='h6' {...props} />
+
+export const Link = styled(tag.a)(
+  space,
+  fontSize,
+  color,
+  themed('Heading'),
+  css
+)
+
+Link.propTypes = {
+  ...space.propTypes,
+  ...fontSize.propTypes,
+  ...color.propTypes,
+}
+
+Link.defaultProps = {
+  color: 'blue'
+}
+
+export const Button = styled(tag.button)({
+  appearance: 'none',
+  display: 'inline-block',
+  textAlign: 'center',
+},
+  space,
+  fontSize,
+  fontWeight,
+  width,
+  color,
+  borders,
+  borderColor,
+  borderRadius,
+  buttonStyle,
+  themed('Button'),
+  css
+)
+
+Button.propTypes = {
+  ...space.propTypes,
+  ...fontSize.propTypes,
+  ...fontWeight.propTypes,
+  ...width.propTypes,
+  ...color.propTypes,
+  ...borders.propTypes,
+  ...borderColor.propTypes,
+  ...borderRadius.propTypes,
+  ...buttonStyle.propTypes,
+}
+
+Button.defaultProps = {
+  fontSize: 'inherit',
+  fontWeight: 'bold',
+  m: 0,
+  px: 3,
+  py: 2,
+  color: 'white',
+  bg: 'blue',
+  border: 0,
+  borderRadius: 2,
+}
+
+export const Image = styled(tag.img)({
+  maxWidth: '100%',
+  height: 'auto'
+},
+  space,
+  color,
+  borderRadius,
+  themed('Image'),
+  css
+)
+
+Image.propTypes = {
+  ...space.propTypes,
+  ...color.propTypes,
+  ...borderRadius.propTypes,
+}
+
+Image.defaultProps = {
+  m: 0
+}
+
+export const Card = styled(tag)(
+  space,
+  fontSize,
+  width,
+  color,
+  borders,
+  borderRadius,
+  boxShadow,
+  backgroundImage,
+  backgroundSize,
+  backgroundPosition,
+  backgroundRepeat,
+  opacity,
+  themed('Card'),
+  css
+)
+
+Card.propTypes = {
+}
+
