f37b390c924174b385ed185e6dcc74baf02e382f
Brent Jackson <jxnblk@gmail.com>
2019-08-04 18:42:48 -0400

    Adjust og metadata

--- packages/docs/src/components/head.js
+++ packages/docs/src/components/head.js
@@ -11,14 +11,13 @@ export default props => {
         lang: 'en-us',
       }}>
       <title>{title}</title>
-      <link
-        rel='icon'
-        href='/icon.png'
-      />
-      <meta
-        name='description'
-        content={pkg.description}
-      />
+      <link rel='icon' href='/icon.png' />
+      <meta name='description' content={pkg.description} />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:site' content='@jxnblk' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={pkg.description} />
+      <meta name='twitter:image' content='https://rebassjs.org/card.png' />
     </Helmet>
   )
 }
