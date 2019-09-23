f78e4f6d79d7c4069993d5e2ba065bf8a3f28942
Brent Jackson <jxnblk@gmail.com>
2019-09-23 20:40:39 +0000

    Add dividers package

---
+++ packages/dividers/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from 'reflexbox'
+
+export const SVG = props =>
+  <Box
+    {...props}
+    as='svg'
+    viewBox='0 0 '
+  />
+
+export const Waves = props =>
+
